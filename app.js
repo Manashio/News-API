3eb24711b41eb5de56a304f4c88cf144e68ac16e
Manash97 <manashbharali79@gmail.com>
2018-01-26 15:56:30 +0530

    Service Worker added

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-
 const apiKey = '36c64372d0aa470d81360c574cef839f';
-
 const main = document.querySelector('main');
-
 window.addEventListener( 'load', e =>{
 	updateNews();
 });
 
-async function updateNews() {
-	const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}
-`);
-	const json = await res.json();
+if('serviceWorker' in navigator){
+	try{
+		navigator.serviceWorker.register('sw.js');
+		console.log(`SW registered`);
+	}catch(error){
 
-	main.innerHTML = json.articles.map(createArticle).join('<br>');
+	}
 }
 
+
+async function updateNews() {
+	const res = await fetch(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${apiKey}
+`);
+	const json = await res.json();
+	main.innerHTML = json.articles.map(createArticle).join('<br>');
+}
 function createArticle(article) {
 	return `
-		<div class="article">
-			<a href="${article.url}">
-				<h2>${article.title}</h2>
-				<img src="${article.urlToImage}">
-				<p>${article.description}</p>
-			</a>
-		</div>
+			<div class="article">
+					<h2>${article.title}</h2>
+					<img src="${article.urlToImage}">
+					<p>${article.description}</p>
+					<a class="btn" href="${article.url}">Read more</a>
+			</div>
 		`;
 }
 
